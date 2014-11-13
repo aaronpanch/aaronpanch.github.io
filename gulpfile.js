e93f9f4a8454696dbca3ccf20674bdfd9c6ad366
Aaron Panchal <aaron.panchal@appfolio.com>
2014-11-12 23:09:40 -0800

    add CSS minifier, rename output directory, clean up tabbing

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
 var gulp = require('gulp');
 
+var destinationDir = './build/';
+
 gulp.task('autoprefixer', function() {
   var postcss = require('gulp-postcss');
   var autoprefixer = require('autoprefixer-core');
 
-  return gulp.src('style.css')
+  return gulp.src('styles.css')
     .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest(destinationDir));
 });
+
+gulp.task('minify', function() {
+  var minifyCSS = require('gulp-minify-css');
+  var rename = require('gulp-rename');
+
+  return gulp.src('./build/styles.css')
+    .pipe(minifyCSS())
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest(destinationDir));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./*.css', ['autoprefixer', 'minify']);
+});
+
+gulp.task('default', ['autoprefixer', 'minify', 'watch']);
