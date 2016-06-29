8e0f1c55f501495aad4960486a1acffb215c6d6f
Aaron Panchal <aaron.panchal@appfolio.com>
2016-06-28 22:32:26 -0700

    Use browser sync to simplify dev

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,25 @@ const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
 
 const destinationDir = './build/';
+
+gulp.task('serve', ['styles'], function() {
+  browserSync.init({
+    server: { baseDir: "./" }
+  });
+
+  gulp.watch('./styles.css', [ 'styles' ]);
+  gulp.watch("./index.html").on('change', browserSync.reload);
+});
 
 gulp.task('styles', function() {
   return gulp.src('styles.css')
     .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
     .pipe(cleanCSS())
-    .pipe(gulp.dest(destinationDir));
+    .pipe(gulp.dest(destinationDir))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./*.css', [ 'styles' ]);
-});
-
-gulp.task('default', [ 'styles' ]);
+gulp.task('default', ['serve']);
