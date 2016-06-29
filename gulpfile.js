6472661e4e5b387b91f7ec593220e0e83f64f3f7
Aaron Panchal <aaron.panchal@appfolio.com>
2016-06-28 22:25:33 -0700

    Update gulp packages and simplify pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,19 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
-var destinationDir = './build/';
+const destinationDir = './build/';
 
-gulp.task('autoprefixer', function() {
-  var postcss = require('gulp-postcss');
-  var autoprefixer = require('autoprefixer-core');
-
+gulp.task('styles', function() {
   return gulp.src('styles.css')
-    .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-    .pipe(gulp.dest(destinationDir));
-});
-
-gulp.task('minify', function() {
-  var minifyCSS = require('gulp-minify-css');
-  var rename = require('gulp-rename');
-
-  return gulp.src('./build/styles.css')
-    .pipe(minifyCSS())
-    .pipe(rename('styles.min.css'))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    .pipe(cleanCSS())
     .pipe(gulp.dest(destinationDir));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./*.css', ['autoprefixer', 'minify']);
+  gulp.watch('./*.css', [ 'styles' ]);
 });
 
-gulp.task('default', ['autoprefixer', 'minify', 'watch']);
+gulp.task('default', [ 'styles' ]);
