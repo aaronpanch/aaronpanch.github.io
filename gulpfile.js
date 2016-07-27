de9400a58c74fb35370d6cfbe438f18a4228eaf6
Aaron Panchal <aaron.panchal@appfolio.com>
2016-07-26 23:37:22 -0700

    Add html minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,30 @@ const gulp = require('gulp')
     , autoprefixer = require('autoprefixer')
     , cleanCSS = require('gulp-clean-css')
     , rename = require('gulp-rename')
+    , htmlmin = require('gulp-htmlmin')
     , browserSync = require('browser-sync').create();
 
-const destinationDir = './build/';
-
-gulp.task('serve', ['styles'], function() {
+gulp.task('serve', ['styles', 'minify'], function() {
   browserSync.init({
     server: { baseDir: "./" }
   });
 
-  gulp.watch('./styles.css', [ 'styles' ]);
-  gulp.watch("./index.html").on('change', browserSync.reload);
+  gulp.watch('./src/styles.css', ['styles']);
+  gulp.watch('./src/index.html', ['minify'], browserSync.reload);
+});
+
+gulp.task('minify', function() {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('.'))
 });
 
 gulp.task('styles', function() {
-  return gulp.src('styles.css')
+  return gulp.src('src/styles.css')
     .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
     .pipe(cleanCSS())
     .pipe(rename('styles.min.css'))
-    .pipe(gulp.dest(destinationDir))
+    .pipe(gulp.dest('.'))
     .pipe(browserSync.stream());
 });
 
