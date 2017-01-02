65eedc39e5a0cec74eebc0250d0d229fda8b98fb
Aaron Panchal <aaron.panchal@appfolio.com>
2017-01-01 22:05:32 -0800

    Set up production build script

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,11 +1,60 @@
 const webpack = require('webpack')
+    , { resolve } = require('path')
     , ExtractTextPlugin = require('extract-text-webpack-plugin')
     , HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
-    path: 'dist',
+    path: resolve(__dirname, 'dist'),
     filename: 'app.js'
-  }
+  },
+  context: resolve(__dirname, 'src'),
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?modules'
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader?modules',
+          'sass-loader'
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    }),
+    new ExtractTextPlugin('styles.css'),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
+      }
+    }),
+  ],
 }
