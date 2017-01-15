fb3e09490d21271926863a8c57a6946618e660ff
Aaron Panchal <aaron.panchal@appfolio.com>
2017-01-14 22:25:01 -0800

    Set up basic webpack dev config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>hello world</h1>, document.getElementById('root') );
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
