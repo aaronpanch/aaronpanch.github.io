67e8501bec72d20dba0222f49c882753c8089c08
Aaron Panchal <aaron.panchal@gmail.com>
2018-08-13 14:51:55 -0700

    Clean up gulp a bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
-const gulp = require('gulp')
-    , postcss = require('gulp-postcss')
-    , autoprefixer = require('autoprefixer')
-    , cleanCSS = require('gulp-clean-css')
-    , rename = require('gulp-rename')
-    , htmlmin = require('gulp-htmlmin')
-    , browserSync = require('browser-sync').create();
+const gulp = require('gulp'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cleanCSS = require('gulp-clean-css'),
+  htmlmin = require('gulp-htmlmin'),
+  browserSync = require('browser-sync').create();
+
+gulp.task('minify', function() {
+  return gulp
+    .src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('styles', function() {
+  return gulp
+    .src('src/styles.css')
+    .pipe(postcss([autoprefixer({ browsers: ['last 2 versions'] })]))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('.'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('build', ['styles', 'minify']);
 
 gulp.task('serve', ['styles', 'minify'], function() {
   browserSync.init({
-    server: { baseDir: "./" }
+    server: { baseDir: './' }
   });
 
   gulp.watch('src/styles.css', ['styles']);
@@ -16,19 +33,4 @@ gulp.task('serve', ['styles', 'minify'], function() {
   gulp.watch('./index.html', browserSync.reload);
 });
 
-gulp.task('minify', function() {
-  return gulp.src('src/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('.'))
-});
-
-gulp.task('styles', function() {
-  return gulp.src('src/styles.css')
-    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-    .pipe(cleanCSS())
-    .pipe(rename('styles.min.css'))
-    .pipe(gulp.dest('.'))
-    .pipe(browserSync.stream());
-});
-
-gulp.task('default', ['serve']);
+gulp.task('default', ['build']);
